69e05d1a3015d02b1370a16c030eb20a1ad47b29
PunkDevRobot <3915056+punk-dev-robot@users.noreply.github.com>
2026-02-25 02:35:36 +0000

    add jest support to neotest

--- config/nvim/lua/plugins/typescript.lua
+++ config/nvim/lua/plugins/typescript.lua
@@ -3,10 +3,17 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "marilari88/neotest-vitest",
+      "haydenmeade/neotest-jest",
     },
     opts = {
       adapters = {
         ["neotest-vitest"] = {
+          cwd = function()
+            return LazyVim.root()
+          end,
+        },
+        ["neotest-jest"] = {
+          jestCommand = "npx jest",
           cwd = function()
             return LazyVim.root()
           end,
